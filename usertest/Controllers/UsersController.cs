aef73e2065cec260b5e7f5625fdc2f87c5cf0841
FranMihovil <fmjukic97@gmail.com>
2023-01-25 22:57:30 +0100

    a

---
+++ usertest/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+
+
+
+using Microsoft.AspNetCore.Mvc;
+
+public class UsersController : Controller
+{
+    [HttpGet]
+    public ActionResult GetData()
+    {
+        var data = new Users { Id = 1, Name = "TestUser" };
+        return Json(data);
+    }
+}
